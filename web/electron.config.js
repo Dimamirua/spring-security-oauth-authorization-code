39a0fe29b5f94266a639a3362c5735869b3b1474
d.miroshnichenko <dimamirua@mail.ru>
2017-08-28 17:01:56 +0300

    electron-config(not finished)

---
+++ web/electron.config.js
@@ -0,0 +1,44 @@
+const {app, BrowserWindow, protocol} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+
+
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/target/dist/index.html'),
+        protocol: 'file:',
+        // slashes: false
+    }));
+
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+
+}
+
+
+app.on('ready', () => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+
+    if (win === null) {
+        createWindow();
+    }
+});
